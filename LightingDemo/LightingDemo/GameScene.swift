461c8d27a32e5380d7e92c58dade9ce139514127
Joachim Grill <joachim@codeandweb.com>
2015-05-18 18:14:38 +0200

    new demo, with animation

--- LightingDemo/LightingDemo/GameScene.swift
+++ LightingDemo/LightingDemo/GameScene.swift
@@ -10,64 +10,108 @@ import SpriteKit
 
 class GameScene: SKScene {
     
-    let light = SKLightNode()
-    let label = SKLabelNode(fontNamed:"Chalkduster")
-    
-    var index = 0
-    let sprites = [ "orange", "crate", "banana", "cherries" ]
+    var _scale: CGFloat = 1.0
+    var _screenH: CGFloat = 640.0
+    var _screenW: CGFloat = 960.0
+    let _fps = 8.0
+    let _light = SKLightNode()
+    var _lightSprite = SKSpriteNode(imageNamed: "Sprites/lightbulb.png")
     
     override func didMoveToView(view: SKView) {
-        /* Setup your scene here */
-        let background = SKSpriteNode(texture: SKTexture(imageNamed:"background"), normalMap: SKTexture(imageNamed:"background_n"))
-        background.lightingBitMask = 1;
-        background.position = CGPointMake(size.width/2, size.height/2)
-        addChild(background)
         
-        label.text = "Touch to create sprites!";
-        label.position = CGPointMake(size.width/2, 60)
-        addChild(label)
+        _screenH = view.frame.height
+        _screenW = view.frame.width
+        _scale = _screenW / 1920
+
+        _light.position = CGPointMake(_screenW - 100, _screenH - 100)
+        _light.falloff = 1
+        _light.ambientColor = UIColor.darkGrayColor()
+        _light.lightColor = UIColor.whiteColor()
+        addChild(_light)
+
+        initBackground()
         
-        light.position = CGPointMake(size.width/2, size.height/2)
-        light.falloff = CGFloat(0.7)
-        light.ambientColor = UIColor.darkGrayColor()
-        light.lightColor = UIColor.lightGrayColor()
-        addChild(light)
+        var animFrames = [SKTexture]()
+        var normals = [SKTexture]()
+        for index in 1...8 {
+            animFrames.append(SKTexture(imageNamed: String(format:"Sprites/character/%02d.png", index)))
+            normals.append(SKTexture(imageNamed: String(format:"Sprites/character/%02d_n.png", index)))
     }
+        let sprite = SKSpriteNode(texture: animFrames[0], normalMap: normals[0])
+    
+        let anim = SKAction.customActionWithDuration(1.0, actionBlock: { node, time in
+            let index = Int((self._fps * Double(time))) % animFrames.count
+            (node as! SKSpriteNode).normalTexture = normals[index]
+            (node as! SKSpriteNode).texture = animFrames[index]
+        })
+        sprite.runAction(SKAction.repeatActionForever(anim));
+        
+        sprite.position = CGPoint(x: _screenW / 2, y: _screenH / 2 - 75.0 * _scale)
+        sprite.setScale(_scale)
+        sprite.lightingBitMask = 1
+            
+                addChild(sprite)
+        
+        _lightSprite.position = _light.position
+        _lightSprite.setScale(_scale * 2)
+        addChild(_lightSprite);
+            }
     
     override func touchesBegan(touches: Set<NSObject>, withEvent event: UIEvent) {
-        /* Called when a touch begins */
-        
         for touch: AnyObject in touches {
-            let location = touch.locationInNode(self)
-            
-            if (index < 4)
-            {
-                let sprite = SKSpriteNode(texture: SKTexture(imageNamed:sprites[index]),
-                                          normalMap: SKTexture(imageNamed: sprites[index] + "_n"))
-            sprite.position = location
-                sprite.lightingBitMask = 1;
-                addChild(sprite)
-                index++;
-                if (index == 4)
-                {
-                    label.text = "Touch to move light!";
-                }
-            }
-            else
-            {
-                light.position = location
-            }
+            _light.position = touch.locationInNode(self)
+            _lightSprite.position = _light.position
         }
     }
-            
     override func touchesMoved(touches: Set<NSObject>, withEvent event: UIEvent) {
         for touch: AnyObject in touches {
-            let location = touch.locationInNode(self)
-            light.position = location
+            _light.position = touch.locationInNode(self)
+            _lightSprite.position = _light.position
         }
     }
    
     override func update(currentTime: CFTimeInterval) {
         /* Called before each frame is rendered */
+    }
+    
+    
+    func initBackground()
+    {
+        backgroundColor = SKColor.blackColor()
+        addBackgroundTile("Sprites/background_01.png", offsetX:0,    speed:50);
+        addBackgroundTile("Sprites/background_01.png", offsetX:1920, speed:50);
+        addBackgroundTile("Sprites/foreground_01.png", offsetX:0,    speed:100, normalsFile:"Sprites/foreground_01_n.png");
+        addBackgroundTile("Sprites/foreground_02.png", offsetX:1920, speed:100, normalsFile:"Sprites/foreground_02_n.png");
+    }
+    
+    
+    func addBackgroundTile(spriteFile: String, offsetX: CGFloat, speed: CGFloat, normalsFile: String = "")
+    {
+        var background:SKSpriteNode
+        if (normalsFile != "")
+        {
+            background = SKSpriteNode(texture: SKTexture(imageNamed:spriteFile), normalMap: SKTexture(imageNamed:normalsFile));
+            background.lightingBitMask = 1
+        }
+        else
+        {
+            background = SKSpriteNode(imageNamed:spriteFile);
+            background.color = _light.ambientColor
+            background.colorBlendFactor = 0.75
+        }
+        
+        let scaledOffsetX = offsetX * _scale
+        let scaledOffsetY = (_screenH - background.size.height * _scale) / 2.0
+        
+        background.anchorPoint = CGPoint(x:0, y:0)
+        background.setScale(_scale)
+        addChild(background);
+        
+        let a1 = SKAction.moveTo(CGPoint(x:scaledOffsetX, y:scaledOffsetY), duration:0)
+        let a2 = SKAction.moveToX(-_screenW,     duration:Double((_screenW + scaledOffsetX) / speed));
+        let a3 = SKAction.moveToX(_screenW,      duration:0)
+        let a4 = SKAction.moveToX(scaledOffsetX, duration:Double((_screenW - scaledOffsetX) / speed));
+
+        background.runAction(SKAction.repeatActionForever(SKAction.sequence([a1, a2, a3, a4])));
     }
 }
